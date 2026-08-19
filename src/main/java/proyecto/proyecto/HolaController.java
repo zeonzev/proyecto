2207ecdb5cdce11b25291fa6edb2d2c899a949d1
zeonzev <zeonzev@gmail.com>
2026-08-19 08:13:11 -0400

    Primer commit

---
+++ src/main/java/proyecto/proyecto/HolaController.java
@@ -0,0 +1,13 @@
+package proyecto.proyecto;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HolaController {
+
+    @GetMapping("/hola")
+    public String hola() {
+        return "Hola mundo";
+    }
+}
